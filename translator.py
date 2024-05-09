d06d4a0d5d5498bfc4930e6f3d61b986a8cc4197
GuruRarquina <155425494+GuruRarquina@users.noreply.github.com>
2024-05-09 19:05:54 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -4,7 +4,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="localhost",        
     user="root",
-    password="shivusql",
+    password="32651",
     database="translator" 
 )
 
